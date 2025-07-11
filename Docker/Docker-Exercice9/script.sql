792d638328e5860e0020b0c1464b715a18979f23
unknown <thomas@m2i.net>
2025-07-11 13:42:49 +0200

    110725

---
+++ Docker/Docker-Exercice9/script.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE exo9;
+
+USE exo9;
+
+CREATE TABLE exo9(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titre VARCHAR(255)
+);
+
+INSERT INTO mytable (titre) VALUES ("docker-exercice9"),("titre2");
